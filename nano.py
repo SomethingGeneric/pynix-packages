bd45e1b2410aa808cf77e01dd6f69aa6aaa4d533
Matt C <mcompton2002@gmail.com>
2018-11-14 20:05:45 -0500

    Feff

--- nano.py
+++ nano.py
@@ -1,18 +1,11 @@
 import os,sys
 
-<<<<<<< HEAD
 
 if len(sys.argv) == 1:
     path = str(sys.argv[1])
 else:
     print("Usage: nano <filename>")
     quit()
-=======
-user = str(sys.argv[1])
-path = str(sys.argv[2])
-
-
->>>>>>> 7c9651f52af7d97d648a3fdc513683959ddf74c6
 
 goal = 'home/'+path
 
