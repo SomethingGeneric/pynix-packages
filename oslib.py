84e3f2149c0011cc25ebbf15c02f3c988acf98b8
Matt C <mcompton2002@gmail.com>
2019-01-01 14:46:33 -0700

    oslib different for package repo dingbat

--- oslib.py
+++ oslib.py
@@ -8,25 +8,9 @@ def verify(path):
 
 class hostmgr:
     def __init__(self):
-        if not verify("hostos"):
-            this = int(input("1) Windows\n2) MacOS/Unix"))
-            if this == 1:
-                save = "\\"
-            elif this == 2:
-                save = "/"
-            else:
-                save = "CANCEL"
-            if save != "CANCEL":
-                f = open("hostos",'w')
-                f.write(save)
-                f.close()
-                self.path = save
-            else:
-                return "User entered invalid number."
-        else:
-            f = open("hostos")
-            self.path = f.read()
-            f.close()
+        f = open("hostos")
+        self.path = f.read()
+        f.close()
     def get(self):
         return self.path
 
